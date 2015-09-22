e7bd974f2769b31fd12f52c79497cc22f050ed0b
Reza Nirumand <nirumand@web.de>
2015-09-23 00:44:06 +0200

    regModelSelected

---
+++ rCode.R
@@ -0,0 +1,17 @@
+library(datasets);library(ggplot2);library(dplyr);library(tidyr);library(GGally)
+dt<-select(mtcars,-qsec) ##removing irrelevant column
+dt<-mutate(dt,cyl=factor(cyl),am=factor(am),vs=factor(vs)) ##adjusting column type
+
+cor(select(dt,-c(cyl,am))) ##getting correlation matrix for only numeric columns.
+
+
+
+g<-ggplot(data=mtcars,aes(y=mpg,x=as.factor(am),fill=as.factor(am)))
+g<-g+geom_violin()
+g<-g+scale_x_discrete(breaks=c(0,1),labels=c("Automaitc","Manaul"))
+g<-g+facet_grid(.~cyl) # having cyl as column
+g<-g+xlab("Transmission type") ##labeling x axis
+g<-g+ylab("Miles/(US) gallon") ## labeling y axis
+g<-g+scale_fill_discrete(name="Transmission type",breaks=c(0,1),labels=c("Automaitc","Manaul")) ## setting correct names for legens.
+g<-g+ggtitle("Comparison of Miles/galon for diffrenct cars\nbased on transmission type and number of cylinders")
+g
