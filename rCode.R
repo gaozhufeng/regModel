f90da8ea8b9ae3cc97ddda65e09039cba0ede343
unknown <nirumandm@STALLMAN.BCIADNET.BCI.TU-DORTMUND.DE>
2015-09-23 16:53:53 +0200

    progress

--- rCode.R
+++ rCode.R
@@ -8,10 +8,20 @@ cor(select(dt,-c(cyl,am))) ##getting correlation matrix for only numeric columns
 
 g<-ggplot(data=mtcars,aes(y=mpg,x=as.factor(am),fill=as.factor(am)))
 g<-g+geom_violin()
-g<-g+scale_x_discrete(breaks=c(0,1),labels=c("Automaitc","Manaul"))
 g<-g+facet_grid(.~cyl) # having cyl as column
 g<-g+xlab("Transmission type") ##labeling x axis
 g<-g+ylab("Miles/(US) gallon") ## labeling y axis
-g<-g+scale_fill_discrete(name="Transmission type",breaks=c(0,1),labels=c("Automaitc","Manaul")) ## setting correct names for legens.
-g<-g+ggtitle("Comparison of Miles/galon for diffrenct cars\nbased on transmission type and number of cylinders")
 g
+g<-g+ggtitle("Comparison of Miles/galon for diffrenct cars\nbased on transmission type and number of cylinders")
+g<-g+scale_x_discrete(breaks=c(0,1),labels=c("Automaitc","Manaul"))
+g<-g+scale_fill_discrete(name="Transmission type",breaks=c(0,1),labels=c("Automaitc","Manaul")) ## setting correct names for legens.
+g
+
+=============
+        
+        
+g<-ggplot(data=dt,aes(x=hp,y=mpg,colour=am))
+g<-g+geom_point(size=6,colour="black")+geom_point(size=4)
+g<-g+xlab("Horsepower")+ylab("Miles per gallon")+ggtitle("Plot of mpg vs hp \ncolor coded by transmission")
+g<-g+scale_x_discrete(breaks=c(0,1),labels=c("Automaitc","Manaul"))
+g<-g+scale_fill_discrete(name="Transmission type",breaks=c(0,1),labels=c("Automaitc","Manaul"))
